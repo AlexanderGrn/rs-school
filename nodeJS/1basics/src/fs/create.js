85a6a63f07350ce62c5d33ee09cf5e8a10aef727
AlexanderGrn <s.green@list.ru>
2022-06-02 16:56:53 +0300

    the ice has broken

--- nodeJS/1basics/src/fs/create.js
+++ nodeJS/1basics/src/fs/create.js
@@ -1,3 +1,15 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
 export const create = async () => {
     // Write your code here 
+    fs.writeFile(
+        path.join('src', 'fs', 'files', 'fresh.txt'),
+        'I am fresh and young',
+        (err) => {
+            if (err) throw err;
+        }
+    );
 };
+
+create();
