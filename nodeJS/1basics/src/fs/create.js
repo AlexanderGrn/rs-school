6ebe989e74459d2497e9a10e48c3d7fc3d515b76
AlexanderGrn <s.green@list.ru>
2022-06-03 09:29:02 +0300

    feat: add "__dirname"

--- nodeJS/1basics/src/fs/create.js
+++ nodeJS/1basics/src/fs/create.js
@@ -1,10 +1,14 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const create = async () => {
     // Write your code here 
     fs.writeFile(
-        path.join('src', 'fs', 'files', 'fresh.txt'),
+        path.join(__dirname, 'files', 'fresh.txt'),
         'I am fresh and young',
         (err) => {
             if (err) throw err;
